1746bcead9dd942e96165c466a87d57e4c432c9f
amarinin <aleksandrqa1@gmail.com>
2024-06-09 14:43:30 +0300

    6th homework - added lesson summary 3

--- conftest.py
+++ conftest.py
@@ -2,8 +2,10 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(scope="session")
+'''В данном случае декоратор @pytest.fixture изменяет поведение функции browser'''
+@pytest.fixture(scope="session") # Это декоратор (функция-обёртка, изменяющая поведение другой функции)
 def browser():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
+    driver = webdriver.Chrome()  # Передали наш драйвер
+    yield driver  # Чтобы наш драйвер мог использовать в функции
+    driver.quit()  # Закрываем драйвер
+    
