f2cdfd5132ab80417cb4b092f2ac628643b3e580
amarinin <aleksandrqa1@gmail.com>
2024-06-25 22:55:28 +0300

    9th homework - 1st task done (test 'test_modal_elements' added) - Refactoring.

--- pages/modal_dialogs.py
+++ pages/modal_dialogs.py
@@ -10,5 +10,8 @@ class ModalDialogsPage(BasePage):
         self.base_url = 'https://demoqa.com/modal-dialogs'
         super().__init__(driver, self.base_url)
 
+        # not_unique_btn - это элементы субменю "Alerts, Frame and Windows"
+        self.btns_submenu = WebElement(driver, '#app > div > div > div > div:nth-child(1) > div > div > div:nth-child(3) > div > ul > .btn, btn-light ')
+
         # not_unique_btn - это элемент с не уникальным локатором, подходящим под все кнопки
         self.btn_not_unique = WebElement(driver, '.btn, btn-light ')  # 35 Элементов на странице
--- tests/tests_hw/test_page_dialogs.py
+++ tests/tests_hw/test_page_dialogs.py
@@ -6,8 +6,15 @@ import time
 
 def test_modal_elements(browser):
     # Данный тест кейс проверяет, что на странице demoqa.com/modal-dialogs 5 кнопок подменю.
+    # Также данный тест кейс проверяет, что на странице 35 элементов с не уникальным локатором.
     modal_dialogs_page = ModalDialogsPage(browser)  # Создали объект страницы
     modal_dialogs_page.visit()  # Перешли на страницу demoqa.com/modal-dialogs
 
+    # Проверяем, что на странице 5 элементов субменю "Alerts, Frame and Windows"
+    assert modal_dialogs_page.btns_submenu.check_count_elements(count=5)
+
     # Проверяем, что на странице 35 элементов с этим локатором
     assert modal_dialogs_page.btn_not_unique.check_count_elements(count=35)
+
+# def test_navigation_modal(browser):
+#     # Данный
